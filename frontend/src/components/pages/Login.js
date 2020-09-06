0b03edd9041b8fb929854799050a8d7ca134a6ab
Godswill <godswill199@gmail.com>
2020-09-06 10:11:38 +0100

    Form validation and submition

--- frontend/src/components/pages/Login.js
+++ frontend/src/components/pages/Login.js
@@ -1,50 +1,61 @@
 import React,{Component} from 'react';
-import LoginBanner from '../../imgs/scratch9ja.png'
-import Logo from '../../imgs/logo.png'
+import LoginBanner from '../../imgs/scratch9ja.png';
+import Logo from '../../imgs/logo.png';
+import {useForm} from 'react-hook-form';
 
 
-export default class Login extends Component {
+
+export default function Login() {
     
-    render() {
-        const loginBg = "url(" + LoginBanner + ") center/cover"
+    const { register, handleSubmit} = useForm();
 
-        return (
+    const loginBg = "url(" + LoginBanner + ") center/cover"
 
-  <div className="page-banner flex justify-center page-header" style={{ background:  loginBg }} >
+    const onSubmit = (values) => {
+        console.log(values);
+    };
+
+
+   return (
+
+ <div className="page-banner flex justify-center page-header" style={{ background:  loginBg }} >
+        
         <div className="text-content container mx-auto p-2 ">
-            <div className="max-w-sm mx-auto my-24 px-5 py-10 rounded shadow-xl">
+       
+           <div className="max-w-sm mx-auto my-24 px-5 py-10 rounded shadow-xl">
     
-                <div className="flex mx-40 logo justify-between mb-8">
-                    <img src={Logo} alt=""/>
-                </div>
+            <div className="flex mx-40 logo justify-between mb-8">
+                <img src={Logo} alt=""/>
+            </div>
     
-                    <form action="#">
-                            <div className="mt-5 ">
-                                <label for="username">Username or Email</label>
-                                <input type="text" id="username" className="block w-full custom-input p-2  rounded "/>
-                            </div>
-             
-                            <div className="mt-5">
-                                <label for="password">Password</label>
-                                <input type="password" id="password" className="block w-full custom-input p-2  rounded " />
-                            </div>
-            
-                            <div className=" mt-8 ">
-                                <input type="submit" value="Login" className="py-3 rounded custom-btn text-center w-full" />
-                                <div>
-                            <a href="#"><span>Forgot password?</span></a>
-                           </div>
-                         </div>
-                            
-                        <br/>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="mt-5 ">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" id="username" className="block w-full custom-input p-2  rounded "  ref={register} />
+        
+                    </div>
+    
+                    <div className="mt-5">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" className="block w-full custom-input p-2  rounded " ref={register} />
+                    </div>
+    
+                    <div className=" mt-8 ">
+                        <input type="submit" value="Login" className="py-3 rounded custom-btn text-center w-full" />
                         <div>
-                            <span>Don't have an account? <a href="/Signup"><strong>Create Account</strong></a> </span>
-                        </div>
-                    </form>
-              </div>
-         </div>
-    </div>
+                       <a href="#"><span>Forgot password?</span></a>
+                  </div>
+                    </div>
+                    
+                  <br/>
+                  <div>
+                      <span>Don't have an account? <a href="/Signup"><strong>Create Account</strong></a> </span>
+                  </div>
+                </form>
+          </div>
+        </div>
+    
+        </div>
         )
     }
-}
 
