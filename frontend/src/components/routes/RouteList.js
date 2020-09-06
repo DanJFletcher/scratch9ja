c00948ea1984e16dab19f9d522a783a3e6bfda25
Godswill <godswill199@gmail.com>
2020-09-06 10:07:29 +0100

    React-hook-form validation and submition

--- frontend/src/components/routes/RouteList.js
+++ frontend/src/components/routes/RouteList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Homepage from '../pages/Homepage'
 import Login from '../pages/Login'
+import Signup from '../pages/Signup';
 
 const routes = {
     "/": () => <Homepage />,
     "/login": () => <Login />, 
+    "/Signup": () => <Signup />, 
 }
 
 export default routes;
