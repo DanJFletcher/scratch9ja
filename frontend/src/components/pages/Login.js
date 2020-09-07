54279bf6cbe748fe672974490d0f2361a5a9e4ad
Godswill <godswill199@gmail.com>
2020-09-07 19:48:19 +0100

    SignUp Form

--- frontend/src/components/pages/Login.js
+++ frontend/src/components/pages/Login.js
@@ -1,17 +1,9 @@
-import React,{Component} from 'react';
+import React  from 'react';
 import LoginBanner from '../../imgs/scratch9ja.png';
 import Logo from '../../imgs/logo.png';
 import {useForm} from 'react-hook-form';
 
-// const Login = () => {
-//     return (
-//         <div>This is the login page</div>
-//     );
-// }
-
-
-export default function Login() {
-    
+const Login = () => {
     const { register, handleSubmit} = useForm();
 
     const loginBg = "url(" + LoginBanner + ") center/cover"
@@ -20,47 +12,48 @@ export default function Login() {
         console.log(values);
     };
 
+    
 
-   return (
-
- <div className="page-banner flex justify-center page-header" style={{ background:  loginBg }} >
-        
-        <div className="text-content container mx-auto p-2 ">
+    return (
        
-           <div className="max-w-sm mx-auto my-24 px-5 py-10 rounded shadow-xl">
+ <div className="page-banner flex justify-center page-header" style={{ background:  loginBg }} >
+   <div className="text-content container mx-auto p-2 ">
+     <div className="max-w-sm mx-auto my-24 px-5 py-10 rounded shadow-xl">
     
             <div className="flex mx-40 logo justify-between mb-8">
                 <img src={Logo} alt=""/>
             </div>
     
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="mt-5 ">
-                        <label htmlFor="username">Username</label>
-                        <input type="text" id="username" className="block w-full custom-input p-2  rounded "  ref={register} />
-        
-                    </div>
-    
-                    <div className="mt-5">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" id="password" className="block w-full custom-input p-2  rounded " ref={register} />
-                    </div>
-    
-                    <div className=" mt-8 ">
-                        <input type="submit" value="Login" className="py-3 rounded custom-btn text-center w-full" />
-                        <div>
-                       <a href="#"><span>Forgot password?</span></a>
-                  </div>
-                    </div>
-                    
-                  <br/>
-                  <div>
-                      <span>Don't have an account? <a href="/Signup"><strong>Create Account</strong></a> </span>
-                  </div>
-                </form>
-          </div>
-        </div>
-    
-        </div>
-        )
-    }
+  <form onSubmit={handleSubmit(onSubmit)}>
 
+            <div className="mt-5 ">
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" className="block w-full custom-input p-2  rounded "  ref={register} />
+            </div>
+    
+            <div className="mt-5">
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" className="block w-full custom-input p-2  rounded " ref={register} />
+            </div>
+    
+            <div className=" mt-8 ">
+                <input type="submit" value="Login" className="py-3 rounded custom-btn text-center w-full" />
+                <div>
+                    <a href="#"><span>Forgot password?</span></a>
+                </div>
+           </div>
+
+              <br/>
+
+            <div>
+                <span>Don't have an account? <a href="/Signup"><strong>Create Account</strong></a> </span>
+            </div>
+
+      </form>
+    </div>
+  </div>
+</div>
+    );
+}
+
+export default Login;
--- frontend/src/components/pages/Signup.js
+++ frontend/src/components/pages/Signup.js
@@ -1,13 +1,68 @@
-import React, { Component } from 'react'
+import React  from 'react';
+import LoginBanner from '../../imgs/scratch9ja.png';
+import Logo from '../../imgs/logo.png';
+import {useForm} from 'react-hook-form';
 
-export class Signup extends Component {
-    render() {
-        return (
-            <div >
-                sign up page
+const SignUp = () => {
+    const { register, handleSubmit} = useForm();
+
+    const loginBg = "url(" + LoginBanner + ") center/cover"
+
+    const onSubmit = (values) => {
+        console.log(values);
+    };
+
+    return (
+       
+ <div className="page-banner flex justify-center page-header" style={{ background:  loginBg }} >
+        
+    <div className="text-content container mx-auto p-2 ">
+       
+        <div className="max-w-sm mx-auto my-24 px-5 py-10 rounded shadow-xl">
+    
+            <div className="flex mx-40 logo justify-between mb-8">
+                <img src={Logo} alt=""/>
             </div>
-        )
-    }
+    
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+                <div className="mt-5 ">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" className="block w-full custom-input p-2  rounded "  ref={register} />
+               </div>
+    
+                   
+               <div className="mt-5">
+                    <label  htmlFor="email">Email Address</label>
+                    <input className="block w-full custom-input p-2  rounded " id="email" type="email" placeholder="Your valid email address" />
+               </div>
+
+               <div className="mt-5">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" className="block w-full custom-input p-2  rounded " ref={register} />
+               </div>
+
+               <div className="mt-5">
+                        <label htmlFor="ConfirmPassword">Confirm Password</label>
+                        <input type="password" id="ConfirmPassword" className="block w-full custom-input p-2  rounded " ref={register} />
+               </div>
+
+    
+               <div className=" mt-8 ">
+                    <input type="submit" value="Register" className="py-3 rounded custom-btn text-center w-full" />        
+               </div>
+                    
+              <br/>
+
+              <div>
+                  <span>Already have an account? <a href="/login"><strong>Login</strong></a> </span>
+              </div>
+              
+           </form>
+        </div>
+    </div>
+ </div> 
+    );
 }
 
-export default Signup
+export default SignUp;
